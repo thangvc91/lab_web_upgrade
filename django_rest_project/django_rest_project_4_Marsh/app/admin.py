6c6dbac7c7c0715a75bb7aa7ebe5e7d17e06f9d0
thang <thangvc91@gmail.com>
2022-05-28 22:56:23 +0700

    update

--- django_rest_project/django_rest_project_4_Marsh/app/admin.py
+++ django_rest_project/django_rest_project_4_Marsh/app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
+from import_export.admin import ExportActionMixin
 # Register your models here.
 from .models import ClientUrl, Register1
-admin.site.register(Register1)
+# admin.site.register(Register1)
 
 admin.site.register(ClientUrl)
+
+
+class Register1Admin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('phone', 'name', 'email')
+
+admin.site.register(Register1,Register1Admin)
